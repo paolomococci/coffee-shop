f392508ba12f521f2037bea8855358a6fff45f6e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-04-02 09:24:24 +0200

    Update ClickedActivity.kt

--- app/src/main/java/local/example/coffeeshop/ClickedActivity.kt
+++ app/src/main/java/local/example/coffeeshop/ClickedActivity.kt
@@ -18,13 +18,19 @@
 
 package local.example.coffeeshop
 
+import android.R.layout.simple_spinner_item
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import android.widget.ArrayAdapter.createFromResource
 import androidx.appcompat.app.AppCompatActivity
-import local.example.coffeeshop.R.id.activity_clicked_text_view
+import local.example.coffeeshop.R.array.phone_labels_array
+import local.example.coffeeshop.R.id.*
 import local.example.coffeeshop.R.layout.activity_clicked
 
-class ClickedActivity : AppCompatActivity() {
+class ClickedActivity :
+    AppCompatActivity(),
+    AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +39,66 @@ class ClickedActivity : AppCompatActivity() {
         val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
         val textView = findViewById<TextView>(activity_clicked_text_view)
         textView.text = message
+        val spinner = findViewById<Spinner>(label_spinner)
+        if (spinner != null) {
+            spinner.onItemSelectedListener = this
+        }
+        val adapter = createFromResource(
+            this,
+            phone_labels_array,
+            simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        if (spinner != null) {
+            spinner.adapter = adapter
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // TODO
+    }
+
+    override fun onItemSelected(
+        parent: AdapterView<*>?,
+        view: View?,
+        position: Int,
+        id: Long
+    ) {
+        val spinnerLabel = parent?.getItemAtPosition(position).toString()
+        displayToast(spinnerLabel)
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+            same_day -> if (checked)
+                displayToast(
+                    getString(
+                        R.string.same_day_messenger_service
+                    )
+                )
+            next_day -> if (checked)
+                displayToast(
+                    getString(
+                        R.string.next_day_messenger_service
+                    )
+                )
+            pick_up -> if (checked)
+                displayToast(
+                    getString(
+                        R.string.pick_up_messenger_service
+                    )
+                )
+            else -> {
+            }
+        }
+    }
+
+    private fun displayToast(spinnerLabel: String) {
+        Toast.makeText(
+            applicationContext,
+            spinnerLabel,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
